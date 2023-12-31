b1e9d8e8a5586d6f51efd59fad051f746e559872
Avinash Tiwari <aviprf10@gmail.com>
2023-12-31 09:55:32 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import './scss/styles.scss';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg">
+          <div className="navbar">
+              <div  classname="col-md-4">
+              <img src={process.env.PUBLIC_URL + '/images/logo.png'}/>
+              </div>
+            <div className="col-md-6">
+              <a Link="#" className="menu">Home</a>
+              <a Link="#" className="menu">Attorneys</a>
+              <a Link="#" className="menu">Practice Areas</a>
+              <a Link="#" className="menu">About Us</a>
+            </div>
+            <div className="col-md-2">
+              <button className='btn btn-outline-secondary'>Contact Us</button>  
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className="container">
+        <div className="row">
+            <div className="col-md-6">
+              <h1 className="bannerheadline">
+                You don’t have to Fight them Alone.
+              </h1>
+              <div className="pdesc">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit blandit, curabitur sodales 
+              conubia ut inceptos faucibus himenaeos tortor eget, 
+              hac massa gravida arcu interdum proin curae.
+              </div>
+              <div class="search">
+                  <i class="envelope-icon"><i className="fa fa-envelope"></i></i>
+                  <input type="text" class="form-control" placeholder="Enter your eamil address"/>
+                  <button class="btn btn-primary">Let's tolk</button>
+                </div>
+            </div>
+            <div className="col-md-6">
+
+            </div>
+        </div>
+      </div>
+    </>
   );
 }
 
